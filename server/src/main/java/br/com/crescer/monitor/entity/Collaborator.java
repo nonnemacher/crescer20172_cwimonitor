fa6db8575ef9e59e594f6789d5748040c53c9d0b
Leonardo Merker Moreira Bork <leonardo.bork@cwi.com.br>
2017-12-08 17:17:17 -0200

    feat(entity): adicionar entidades ao projetio

---
+++ server/src/main/java/br/com/crescer/monitor/entity/Collaborator.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.monitor.entity;
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Email;
+import org.springframework.format.annotation.DateTimeFormat;
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
+
+
+/**
+ *
+ * @author Leonardo Bork
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "COLLABORATOR")
+public class Collaborator implements Serializable {
+
+    private static final String SQ_NAME = "SQ_COLLABORATOR";
+
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = SQ_NAME)
+    @SequenceGenerator(name = SQ_NAME, sequenceName = SQ_NAME, allocationSize = 1)
+    @Column(name = "ID_COLLABORATOR")
+    private Long id;
+
+    @Size(max = 255)
+    @NotNull(message = "Field must not be null")
+    @Basic(optional = false)
+    @Column(name = "NAME", nullable = false, length = 255)
+    private String name;
+
+
+    @Size(max = 512)
+    @Basic(optional = true)
+    @Column(name = "PROFILE_PIC", nullable = true, length = 512)
+    private String profilePic;
+
+    @Email
+    @NotNull(message = "Field must not be null")
+    @Basic(optional = false)
+    @Column(name = "DS_EMAIL", unique = true, nullable = false)
+    private String email;
+
+    @NotNull(message = "Field must not be null")
+    @Basic(optional = false)
+    @Column(name = "BIRTHDATE", nullable = false)
+    @DateTimeFormat(iso = DATE)
+    private LocalDate birthdate;
+
+    @NotNull(message = "Field must not be null")
+    @Size(max = 400)
+    @Basic(optional = false)
+    @Column(name = "DS_PASS", nullable = false)
+    private String pass;
+
+}
---
+++ server/src/main/java/br/com/crescer/monitor/entity/MachineTag.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.monitor.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Email;
+
+/**
+ *
+ * @author Leonardo Bork
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "MACHINE_TAG")
+public class MachineTag implements Serializable {
+
+    private static final String SQ_NAME = "SQ_MACHINE_TAG";
+
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = SQ_NAME)
+    @SequenceGenerator(name = SQ_NAME, sequenceName = SQ_NAME, allocationSize = 1)
+    @Column(name = "ID_MACHINE_TAG")
+    private Long id;
+
+    @Email
+    @NotNull(message = "Field must not be null")
+    @Basic(optional = false)
+    @Column(name = "DESCRIPTION", unique = true, nullable = false)
+    private String description;
+
+    @ManyToMany
+    @JoinTable(name = "MACHINE_TAG_REGISTER",
+            joinColumns = {
+                @JoinColumn(name = "FK_TAG")},
+            inverseJoinColumns = {
+                @JoinColumn(name = "FK_REGISTER")})
+    private List<MachineRegister> machines;
+}
